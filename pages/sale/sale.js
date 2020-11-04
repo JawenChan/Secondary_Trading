964ea73c032b6ed58e34abb81f9590ca8f8783fb
jawen_chan <1277284476@qq.com>
2020-11-04 18:19:43 +0800

    Sale界面

--- pages/sale/sale.js
+++ pages/sale/sale.js
@@ -5,8 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    picLists:[]
   },
+  addPictures(e){
+    let that=this
+    console.log("点击了上传",e.detail)
+      for(let i in e.detail.current){
+        this.picLists.push({ path: e.detail.current[i].url });
+        this.setData({ 
+          picLists
+        });
+        console.log("点击了",picLists[i])
+      }
+      
+
+    // for(let i in e.detail.current)
+    //   wx.cloud.uploadFile({
+    //     cloudPath:new Date().getTime()+ '.png', // 上传至云端的路径
+    //     filePath: e.detail.current[i].url, // 小程序临时文件路径
+    //     success: res => {
+    //       // 返回文件 ID
+    //       console.log("上传成功",res.fileID)
+    //       this.setData({
+    //         imgUrl:res.fileID
+    //       })
+    //     },
+    //     fail: console.error
+    //   })
+    
+  },
+  
 
   /**
    * 生命周期函数--监听页面加载
